718c40e6f1513ee0d72e3705b31a7aab40b07658
Abdelfattah Atef <abdelfattah0atef@gmail.com>
2023-11-09 18:32:50 +0400

    Create basic structure of the app

---
+++ app/components/AddToCartBtn.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCartBtn = () => {
+  return (
+    <button onClick={() => console.log("Add to cart clicked")}>
+      Add to cart
+    </button>
+  );
+};
+
+export default AddToCartBtn;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCartBtn from "./AddToCartBtn";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h3>Product name</h3>
+      <AddToCartBtn />
+    </div>
+  );
+};
+
+export default ProductCard;
