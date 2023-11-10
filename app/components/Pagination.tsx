d607225c984750b2d7bfeed2611524c66101ff29
Abdelfattah Atef <abdelfattah0atef@gmail.com>
2023-11-10 12:21:33 +0400

    Add products page pagination functionality

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+interface PaginationProps {
+  numberOfPages: number;
+  total: number;
+}
+
+const disabledClasses =
+  "bg-gray-200 cursor-not-allowed hover:bg-gray-200 hover:text-gray-500";
+
+const Pagination = ({ numberOfPages, total }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const skip = searchParams.get("skip") ?? "0";
+  const limit = searchParams.get("limit") ?? "9";
+
+  const isPrevBtnDisabled = Number(skip) === 0;
+  const isNextBtnDisabled = Number(skip) >= total - Number(limit);
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="flex items-center -space-x-px h-10 text-base">
+        <button
+          disabled={isPrevBtnDisabled}
+          onClick={() =>
+            router.push(
+              `/products?skip=${Number(skip) - Number(limit)}&limit=${limit}`
+            )
+          }
+          className={`flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer ${
+            isPrevBtnDisabled && disabledClasses
+          }`}
+        >
+          <span className="sr-only">Previous</span>
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </button>
+        {Array.from(Array(numberOfPages).keys()).map((index: number) => (
+          <button
+            onClick={() =>
+              router.push(
+                `/products?skip=${index * Number(limit)}&limit=${limit}`
+              )
+            }
+            className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 cursor-pointer"
+          >
+            {index + 1}
+          </button>
+        ))}
+
+        <button
+          disabled={isNextBtnDisabled}
+          onClick={() =>
+            router.push(
+              `/products?skip=${Number(skip) + Number(limit)}&limit=${limit}`
+            )
+          }
+          className={`flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer ${
+            isNextBtnDisabled && disabledClasses
+          }`}
+        >
+          <span className="sr-only">Next</span>
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </button>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,29 +1,48 @@
+import Pagination from "../components/Pagination";
 import ProductCard from "../components/ProductCard";
 
-const getProducts = async () => {
-  try {
-    const res = await fetch("https://dummyjson.com/products");
+interface ProductPageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
+const getProducts = async ({
+  skip,
+  limit,
+}: {
+  skip: string | string[];
+  limit: string | string[];
+}) => {
+  try {
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
+    );
     const data = await res.json();
     return data;
-  } catch (error) {
-    console.log("###Error", error);
-  }
+  } catch (error) {}
 };
 
-const ProductsPage = async () => {
-  const data = await getProducts();
+const ProductsPage = async ({ searchParams }: ProductPageProps) => {
+  const skip = searchParams["skip"] ?? "0";
+  const limit = searchParams["limit"] ?? "9";
+  const data = await getProducts({ skip, limit });
+
+  const numberOfPages = Math.ceil(data.total / Number(limit));
+
   if (!data) {
     <div className="p-5">Loading...</div>;
   }
   return (
     <div className="p-5">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-12">
         {data?.products?.map((product: any) => (
           <div key={product.id} className="col-span-1">
             <ProductCard product={product} />
           </div>
         ))}
+      </div>
+
+      <div className="flex items-center justify-center">
+        <Pagination numberOfPages={numberOfPages} total={data.total} />
       </div>
     </div>
   );
